e47f57d8db2ce1111ff41725d9d5a93ad85c8dc1
Farshad-class <fkazempour543@gmail.com>
2022-05-24 13:04:31 +0430

    Last Update

--- ATMfs.cpp
+++ ATMfs.cpp
@@ -7,10 +7,12 @@
 #pragma warning(disable : 4996)
 using namespace std;
 string name, family, Cardnumber, cardnumber, ca;
-int balance, cardpass, pass, pass1, pass2;
-int balance2, cardpass2;
+string  cardpass, pass, pass1, pass2;
+int balance, balance2, cardpass2;
 int a, b, k, en, ex, id, fi;
 bool flag = 0;
+bool Activeflag = 1;
+bool lettransfer = 1;
 void chckbalance();
 void menu1();
 void checkpass();
@@ -18,6 +20,7 @@ void fill1();
 void transferMoney1();
 void changePass1();
 void exit();
+void exit2();
 int main()
 {
 	fill1();
@@ -80,8 +83,27 @@ void checkpass()
 	{
 		cout << "Please enter your card number : ";
 		cin >> cardnumber;
+
+		do {
+			if (cardnumber.length() != 16)
+			{
+				cout << "your card number is not correct " << endl;
+				cout << "Please enter your card number : ";
+				cin >> cardnumber;
+			}
+		} while (cardnumber.length() != 16);
+
+
 		cout << "Please enter your card pass : ";
 		cin >> pass;
+		do {
+			if (pass.length() != 4)
+			{
+				cout << "your password should be 4 digit" << endl;
+				cout << "Please enter your card pass : ";
+				cin >> pass;
+			}
+		} while (pass.length() != 4);
 		ifstream open("bank.txt");
 		while (open >> id)
 		{
@@ -90,39 +112,88 @@ void checkpass()
 			open >> balance;
 			open >> cardpass;
 			open >> Cardnumber;
-			if (cardnumber == Cardnumber)
+			open >> Activeflag;
+			if (Activeflag != 0)
 			{
-				if (pass == cardpass)
+				if (cardnumber == Cardnumber)
 				{
-					fi = id;
-					menu1();
-
-
-					break;
-				}
-				else
-				{
-					cout << "You enter mistake more than 1 times. " << endl;
-					for (int j = 2; j <= 3; j++)
+					if (pass == cardpass)
 					{
-						cout << "Please enter your card pass : ";
-						cin >> pass;
-						if (pass != cardpass)
+						fi = id;
+						menu1();
+
+
+						break;
+					}
+					else
+					{
+						cout << "You enter mistake more than 1 times. " << endl;
+						for (int j = 2; j <= 3; j++)
 						{
-							cout << "You enter mistake " << j << " times." << endl;
-						}
-						else
-						{
-							fi = id;
-							menu1();
-							break;
-						}
-						if (j == 3)
-						{
-							cout << "You enter mistake more than" << j << " times. Your Card is blocked" << endl;
+							cout << "Please enter your card pass : ";
+							cin >> pass;
+							if (pass != cardpass)
+							{
+								cout << "You enter mistake " << j << " times." << endl;
+							}
+							else
+							{
+								fi = id;
+								menu1();
+								break;
+							}
+							if (j == 3)
+							{
+								ifstream open("bank.txt");
+								fstream fobjtemp;
+								fobjtemp.open("temp.txt", ios::out);
+								while (open >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
+								{
+									fobjtemp << id << endl;
+									fobjtemp << name << endl;
+									fobjtemp << family << endl;
+									fobjtemp << balance << endl;
+									fobjtemp << cardpass << endl;
+									fobjtemp << Cardnumber << endl;
+									if (cardnumber == Cardnumber)
+									{
+										Activeflag = 0;
+										fobjtemp << Activeflag << endl;
+
+									}
+									else
+									{
+										Activeflag = 1;
+										fobjtemp << Activeflag << endl;
+									}
+								}
+								ifstream openn("temp.txt");
+								fstream fobj;
+								fobj.open("bank.txt", ios::out);
+								while (openn >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
+								{
+
+									fobj << id << endl;
+									fobj << name << endl;
+									fobj << family << endl;
+									fobj << balance << endl;
+									fobj << cardpass << endl;
+									fobj << Cardnumber << endl;
+									fobj << Activeflag << endl;
+
+								}
+								cout << "You enter mistake more than" << j << " times. Your Card is blocked" << endl;
+								exit2();
+							}
+
 						}
 					}
 				}
+			}
+			else
+			{
+				cout << "Your card is blocked for security resoan, Please contact with bank administrator" << endl;
+				exit2();
 			}
 		}
 	}
@@ -138,85 +209,119 @@ void transferMoney1()
 	}
 	else
 	{
-		cout << "Please enter new card : " << endl;
+		cout << "Please enter new card : ";
 		cin >> ca;
 		if (ca.length() != 16)
 		{
-			cout << "your card number is not correct";
+			cout << "your card number is not correct. " << endl;
 			cout << "Please enter new card : ";
 			cin >> ca;
 		}
-		else
+		else if (ca != Cardnumber)
 		{
-			ifstream open("bank.txt");
-			fstream temp;
-			temp.open("temp.txt", ios::out);
-			while (open >> id >> name >> family >> balance >> cardpass >> Cardnumber)
+			fstream fileobj2;
+			fileobj2.open("bank.txt", ios::in);
+			while (fileobj2 >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
 			{
-				temp << id << endl;
-				temp << name << endl;
-				temp << family << endl;
 				if (ca == Cardnumber)
 				{
-					balance += en;
-					flag = 1;
-					temp << balance << endl;
+					fileobj2 << id << endl;
+					fileobj2 << name << endl;
+					fileobj2 << family << endl;
+					fileobj2 << balance << endl;
+					fileobj2 << cardpass << endl;
+					fileobj2 << Cardnumber << endl;
+					fileobj2 << Activeflag << endl;
+					if (Activeflag == 0)
+					{
+						lettransfer = 0;
+						cout << "Sorry, Destination card is block for security resaon." << endl;
+						break;
+					}
 
 				}
 				else
 				{
-					temp << balance << endl;
+					lettransfer = 1;
 				}
-				temp << cardpass << endl;
-				temp << Cardnumber << endl;
-
 			}
-			ifstream openn("temp.txt");
-			fstream bank1;
-			bank1.open("bank.txt", ios::out);
-			while (openn >> id >> name >> family >> balance >> cardpass >> Cardnumber)
+			if (lettransfer != 0)
 			{
-				if (id != fi)
+				ifstream open("bank.txt");
+				fstream temp;
+				temp.open("temp.txt", ios::out);
+				while (open >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
 				{
-					bank1 << id << endl;
-					bank1 << name << endl;
-					bank1 << family << endl;
-					bank1 << balance << endl;
-					bank1 << cardpass << endl;
-					bank1 << Cardnumber << endl;
-				}
-				else
-				{
-					bank1 << id << endl;
-					bank1 << name << endl;
-					bank1 << family << endl;
-					balance -= en;
-					bank1 << balance << endl;
-					bank1 << cardpass << endl;
-					bank1 << Cardnumber << endl;
+					temp << id << endl;
+					temp << name << endl;
+					temp << family << endl;
+					if (ca == Cardnumber)
+					{
+						balance += en;
+						flag = 1;
+						temp << balance << endl;
 
+					}
+					else
+					{
+						temp << balance << endl;
+					}
+					temp << cardpass << endl;
+					temp << Cardnumber << endl;
+					temp << Activeflag << endl;
+
+				}
+				ifstream openn("temp.txt");
+				fstream bank1;
+				bank1.open("bank.txt", ios::out);
+				while (openn >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
+				{
+					if (id != fi)
+					{
+						bank1 << id << endl;
+						bank1 << name << endl;
+						bank1 << family << endl;
+						bank1 << balance << endl;
+						bank1 << cardpass << endl;
+						bank1 << Cardnumber << endl;
+						bank1 << Activeflag << endl;
+					}
+					else
+					{
+						bank1 << id << endl;
+						bank1 << name << endl;
+						bank1 << family << endl;
+						balance -= en;
+						bank1 << balance << endl;
+						bank1 << cardpass << endl;
+						bank1 << Cardnumber << endl;
+						bank1 << Activeflag << endl;
+
+					}
 				}
 			}
 		}
-	}
-	fstream fileobj;
-	fileobj.open("bank.txt", ios::in);
-	while (fileobj >> id >> name >> family >> balance >> cardpass >> Cardnumber)
-	{
-		if (id == fi)
+		fstream fileobj;
+		fileobj.open("bank.txt", ios::in);
+		while (fileobj >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
 		{
-			fileobj << id << endl;
-			fileobj << name << endl;
-			fileobj << family << endl;
-			fileobj << balance << endl;
-			fileobj << cardpass << endl;
-			fileobj << Cardnumber << endl;
-			break;
+			if (id == fi)
+			{
+				fileobj << id << endl;
+				fileobj << name << endl;
+				fileobj << family << endl;
+				fileobj << balance << endl;
+				fileobj << cardpass << endl;
+				fileobj << Cardnumber << endl;
+				fileobj << Activeflag << endl;
+				break;
+			}
 		}
 	}
 	if (flag == 1)
 	{
 		cout << en << " IRR successfully transfer. " << endl;
+		flag = 0;
 	}
 	cout << "For return to main menu, please enter number 5 : ";
 	cin >> k;
@@ -242,7 +347,7 @@ void changePass1()
 	cin >> pass;
 	fstream fileobj;
 	fileobj.open("bank.txt", ios::in);
-	while (fileobj >> id >> name >> family >> balance >> cardpass >> Cardnumber)
+	while (fileobj >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
 	{
 		if (id == fi)
 		{
@@ -252,6 +357,7 @@ void changePass1()
 			fileobj << balance << endl;
 			fileobj << cardpass << endl;
 			fileobj << Cardnumber << endl;
+			fileobj << Activeflag << endl;
 			break;
 		}
 	}
@@ -292,7 +398,7 @@ void changePass1()
 			ifstream open1("bank.txt");
 			fstream temp;
 			temp.open("temp.txt", ios::out);
-			while (open1 >> id >> name >> family >> balance >> cardpass >> Cardnumber)
+			while (open1 >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
 			{
 				temp << id << endl;
 				temp << name << endl;
@@ -309,11 +415,12 @@ void changePass1()
 					temp << cardpass << endl;
 				}
 				temp << Cardnumber << endl;
+				temp << Activeflag << endl;
 			}
 			ifstream openn1("temp.txt");
 			fstream bank11;
 			bank11.open("bank.txt", ios::out);
-			while (openn1 >> id >> name >> family >> balance >> cardpass >> Cardnumber)
+			while (openn1 >> id >> name >> family >> balance >> cardpass >> Cardnumber >> Activeflag)
 			{
 				bank11 << id << endl;
 				bank11 << name << endl;
@@ -321,6 +428,7 @@ void changePass1()
 				bank11 << balance << endl;
 				bank11 << cardpass << endl;
 				bank11 << Cardnumber << endl;
+				bank11 << Activeflag << endl;
 			}
 			cout << "Your password change successfully." << endl;
 			checkpass();
@@ -333,6 +441,10 @@ void exit()
 	{
 		cout << "Thanks you and good bye. ";
 	}
+}
+void exit2()
+{
+	cout << "Thanks you and good bye. ";
 }
 
 
